ee6de082c6d5c93f2ff564d5883b94e44b0efcbd
nsaibirni <nsairobby@gmail.com>
2016-10-02 08:44:45 +0200

    Module One Solution Upload

---
+++ module1-solution/app.js
@@ -0,0 +1,27 @@
+(function(){
+  'use strict';
+  angular.module('LunchCheck',[])
+  .controller('LunchCheckController', LunchCheckController);
+
+  LunchCheckController.$inject = ['$scope'];
+
+  function LunchCheckController($scope){
+    $scope.lunchMenu = "";
+    $scope.message = "";
+
+    $scope.showMessage = function(){
+      if($scope.lunchMenu == ''){
+        $scope.message = "Please enter data first";
+        return;
+      }
+      alert($scope.lunchMenu);
+      var lunchItems = $scope.lunchMenu.split(",");
+      lunchItems = lunchItems.filter(v=>v != '');
+      if(lunchItems.length > 3)
+        $scope.message = "Too Much";
+      else
+        $scope.message = "Enjoy";
+    }
+  }
+
+})();
